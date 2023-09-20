96768e502506346d2c22755db531777200b0d6ae
Joseph <ralphlsulit@gmail.com>
2023-09-20 23:10:55 +0800

    feat: added messageCount and isLoggedIn props in Greet Component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Greet name='Ralph' />
+      <Greet name='Ralph' messageCount={20} isLoggedIn={true} />
     </div>
   );
 }
--- src/components/Greet.component.tsx
+++ src/components/Greet.component.tsx
@@ -2,12 +2,20 @@ import { Fragment } from "react"
 
 type GreetProps = {
   name: string
+  messageCount: number 
+  isLoggedIn: boolean
+
 }
 
 export const Greet = (props: GreetProps) => {
   return (
     <Fragment>
-      <h2>Welcome, {props.name}!</h2>
+      {
+        props.isLoggedIn
+          ? `Welcome, ${props.name}! You have ${props.messageCount} unread messages.`
+          : `Welcome, Guest`
+      }
+      
     </Fragment>
   )
 };
