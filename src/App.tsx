40a39a3484f675e2e508ef682ac3cd978a0c49c7
Joseph <ralphlsulit@gmail.com>
2023-09-21 00:55:17 +0800

    feat: added status component (Advanced Props)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Greet } from './components/Greet.component';
 import { Person } from './components/Person.component';
 import { PersonList } from './components/PersonList.component';
+import { Status } from './components/Status';
 
 import './App.css';
 
@@ -30,6 +31,7 @@ function App() {
       <Greet name='Bruce' messageCount={20} isLoggedIn={true} />
       <Person name={personName} />
       <PersonList names={nameList} />
+      <Status status='loading' /> 
     </div>
   );
 }
